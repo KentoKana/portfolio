9639501909bf32ed0493a2f96286c12bd7fe3f8f
Kento Kanazawa <kentokanazawa@yahoo.com>
2019-05-02 23:17:01 -0400

    added logic for hamburger menu

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,52 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container'
-import ScrollableAnchor from 'react-scrollable-anchor';
 
-const Navigation = (props) => {
+class Navigation extends Component {
 
-    const navItems =
-        [
-            {
-                id: 'nav__home',
-                name: 'Home',
-                goTo: 'home',
-            },
-            {
-                id: 'nav__projects',
-                name: 'Projects',
-                goTo: 'projects',
-            },
-            {
-                id: 'nav__coreSkills',
-                name: 'Core Skills',
-                goTo: 'skills',
-            },
-            {
-                id: 'nav__contact',
-                name: 'Contact',
-                goTo: 'contact'
-            },
-        ]
-    return (
-        <Container>
-            <nav className='navigation'>
-                <ul>
-                    {navItems.map((n, index) =>
-                        <li
-                            key={index}
-                            id={index}
-                        >
-                            <a className="nav__link" href={'#' + n.goTo}> {n.name} </a>
-                        </li>
-                    )}
-                    {/* <li>Home</li>
-                    <li>Projects</li>
-                    <li>Core Skills</li>
-                    <li>Contact</li> */}
-                </ul>
-            </nav>
-        </Container>
-    )
+    constructor(props) {
+        super(props);
+        this.state = {
+            navVisible: false,
+            navBarClassName: 'navigation',
+            navLinkClicked: false,
+        }
+        this.navItems =
+            [
+                {
+                    id: 'nav__home',
+                    name: 'Home',
+                    goTo: 'home',
+                },
+                {
+                    id: 'nav__projects',
+                    name: 'Projects',
+                    goTo: 'projects',
+                },
+                {
+                    id: 'nav__coreSkills',
+                    name: 'Core Skills',
+                    goTo: 'skills',
+                },
+                {
+                    id: 'nav__contact',
+                    name: 'Contact',
+                    goTo: 'contact'
+                },
+            ]
+    }
+
+    handleMobileNavClick = () => {
+        this.setState((prevState) => {
+            prevState.navVisible = !this.state.navVisible;
+        });
+        if (!this.state.navVisible) {
+            this.setState({
+                navBarClassName: 'navigation nav-visible',
+            });
+        } else {
+            this.setState({
+                navBarClassName: 'navigation',
+            });
+        }
+    }
+
+    render() {
+        return (
+            <Container>
+                <nav className={this.state.navBarClassName}>
+                    <i className="fas fa-bars" id="hamburger-menu" onClick={this.handleMobileNavClick}></i>
+                    <ul>
+                        {this.navItems.map((n, index) =>
+                            <li
+                                key={index}
+                                id={index}
+                            >
+                                <a onClick={this.handleMobileNavClick} className="nav__link" href={`#${n.goTo}`}> {n.name} </a>
+                            </li>
+                        )}
+                    </ul>
+                </nav>
+            </Container>
+        )
+    }
 }
 
 export default Navigation;
